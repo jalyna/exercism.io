ba23f83bc9c9281beff2ea258f6099bb99fa2fe6
Jalyna <jalyna.schroeder@gmail.com>
2014-12-01 19:45:43 +0100

    Add submissions

--- lib/app/routes/dashboard.rb
+++ lib/app/routes/dashboard.rb
@@ -14,6 +14,12 @@ module ExercismWeb
         erb :"dashboard/teams", locals: { profile: Profile.new(current_user) }
       end
 
+      get '/dashboard/submissions' do
+        please_login
+
+        erb :"dashboard/submissions", locals: { profile: Profile.new(current_user) }
+      end
+
     end
   end
 end
